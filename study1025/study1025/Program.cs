8da708d926cac006cec40903d6d0ed170a8ee220
jibin0518 <114150533+jibin0518@users.noreply.github.com>
2022-10-25 21:10:49 +0900

    10.25 공부한 내용

---
+++ study1025/study1025/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace study1025
+{
+    internal class Proram
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+
+            Console.Write("수행할 잡억을 입력하세요");
+            string read = Console.ReadLine();
+
+            Console.Write("a 입력 : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b 입력 : ");
+            int b = int.Parse(Console.ReadLine());
+            if (read == "+")
+            {
+                Console.WriteLine("a + b : " + test.add(a, b));
+            }
+            if (read == "-")
+            {
+                Console.WriteLine("a - b : " + test.minus(a, b));
+            }                                   
+            if (read == "*")
+            {
+                Console.WriteLine("a * b : " + test.multi(a, b));
+            }
+            if (read == "/")
+            {
+                Console.WriteLine("a / b : " + test.div(a, b));
+            }
+
+            Console.Read();
+        }
+
+    }
+}
---
+++ study1025/study1025/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study1025
+{
+    internal class Test
+    {
+        public int add(int a,int b)//int float char string double boolean
+        {
+            int result = a + b;
+            return result;
+        }
+
+        public int minus(int a, int b)
+        {
+            int result = a - b;
+            return result;
+        }
+
+        public int multi(int a,int b)
+        {
+            int result = a * b;
+            return result;
+        }
+
+        public int div(int a,int b)
+        {
+            int result = a / b;
+            return result;
+        }
+    }
+}
